faf4bc464d1634fbadf5eb5e838aea22c74159c0
user <irin-shkuratov8@yandex.ru>
2025-05-26 16:10:42 +0300

    /

---
+++ GalvaBot/src/main/java/com/example/GalvaBot/GalvaBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.GalvaBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GalvaBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GalvaBotApplication.class, args);
+	}
+
+}
---
+++ GalvaBot/src/test/java/com/example/GalvaBot/GalvaBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GalvaBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GalvaBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
